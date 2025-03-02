8fa80a96db2d361527b8137aa9a97709903a9f65
torytang <torytang.025@gmail.com>
2025-03-02 14:02:37 +0800

    feat: init

--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react"
 
-import "./style.css"
-
 function IndexNewtab() {
   const [data, setData] = useState("")
 
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,19 @@
+import { Button } from "@/components/ui/button"
 import { useState } from "react"
+
+import "@/styles/globals.css"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
+    <div className="p-4 w-96">
       <h1>
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <footer>Crafted by @PlasmoHQ</footer>
+      <Button size="sm">1123</Button>
     </div>
   )
 }
